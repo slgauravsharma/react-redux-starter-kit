a583f5b93da1eb8a26bd23c6d454710ac0f7d862
Gaurav <sl.gauravsharma@gmail.com>
2018-10-14 23:23:22 +0530

    Removed unused path in routes.

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,9 @@
 import App from "./components/App";
-import StudentDashboard from "./components/student/StudentDashboard";
 
 const routes = [
   {
     path: "/",
     component: App
   },
-  {
-    path: "/dashboard",
-    component: StudentDashboard
-  }
 ];
 export default routes;
